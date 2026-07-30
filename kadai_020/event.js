da8d889bf3fe89a2ab76fd9b6149379e97bebfac
nipa-sanfre <ryuuking0@gmail.com>
2026-07-30 13:40:05 +0900

    Update event.js

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,7 +1,7 @@
 const btn = document.getElementById('btn');
 
-btn.addEventListener('click', () => {
-    const text = document.getElementById('text');
+const text = document.getElementById('text');
 
-    text.textContent = 'ボタンをクリックしました'
+btn.addEventListener('click', () => {
+    text.textContent = 'ボタンをクリックしました';
 })
